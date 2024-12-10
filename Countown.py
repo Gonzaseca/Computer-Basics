f34c0e98af2893fda5b1c2536e5c39b3487d954c
Gonzaseca <gonzaloserrano01@icloud.com>
2024-12-10 08:58:58 +0100

    Add files via upload

--- Countown.py
+++ Countown.py
@@ -1,8 +1,13 @@
+#Initial number
 number=10
+#Different wires
 wires=['green','red','yellow','blue']
+#This loop allows us to ask the user every "second" 
 while number>0:
     print(number)
+    #This variable stores the wires the user wants to cut
     cut=input(f"Cut one {wires}" )
+    #Using the elements of the list I can differentiate the wires and give different responses to the being cut
     if cut== wires[0]:
         print("Well done")
         break
@@ -15,11 +20,15 @@ while number>0:
     if cut== "blue":
         number-= 1
         continue
+    #In case the user doesn't enter a valid input or skips the question this message appears
     else:
         if number> 1:
             print("Are you going to cut or not?")
+        #This messages appears if the last input is invalid
         else:
             print("You could at least try")
+    #This is what makes the countdown work
     number-= 1
+#Final explosion
 else:
     print("¡BOOM!")
